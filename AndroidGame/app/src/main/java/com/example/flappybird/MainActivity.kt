036544fc7240a898e311456d6d2f7b52a96027e4
Vicky Kumar <imvickykumar999@gmail.com>
2024-08-18 17:27:12 +0530

    errors
    Unable to delete directory 'C:\Users\surface\Documents\GitHub\Floor-is-Lava-Game\AndroidGame\app\build\intermediates\merged_res_blame_folder\debug\mergeDebugResources\out'
      Failed to delete some children. This might happen because a process has files open or has its working directory set in the target directory.
      - C:\Users\surface\Documents\GitHub\Floor-is-Lava-Game\AndroidGame\app\build\intermediates\merged_res_blame_folder\debug\mergeDebugResources\out\multi-v2
      - C:\Users\surface\Documents\GitHub\Floor-is-Lava-Game\AndroidGame\app\build\intermediates\merged_res_blame_folder\debug\mergeDebugResources\out\single

--- AndroidGame/app/src/main/java/com/example/flappybird/MainActivity.kt
+++ AndroidGame/app/src/main/java/com/example/flappybird/MainActivity.kt
@@ -1,28 +1,28 @@
 package com.example.flappybird
 
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.flappybird.ui.theme.FlappyBirdTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FlappyBirdTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    WebViewContainer(
+                        url = "https://imvickykumar999.w3spaces.com/",
+                        modifier = Modifier.padding(innerPadding) // Apply innerPadding here
                     )
                 }
             }
@@ -31,17 +31,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun WebViewContainer(url: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        },
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlappyBirdTheme {
-        Greeting("Android")
-    }
 }
