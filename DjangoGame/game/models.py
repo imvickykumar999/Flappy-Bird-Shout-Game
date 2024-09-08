136948f7091d87b93176a7b6122b92a6879661dd
Vicky Kumar <imvickykumar999@gmail.com>
2024-09-08 20:54:58 +0530

    Delete DjangoGame/game directory

--- DjangoGame/game/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class HighScore(models.Model):
-    player_name = models.CharField(max_length=100, blank=True, null=True)  # Optional field for player name
-    score = models.IntegerField(default=0)
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.player_name or "Anonymous"} - {self.score}'
